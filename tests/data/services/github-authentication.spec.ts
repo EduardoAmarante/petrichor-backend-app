103b87acdd2080f4a0ae20adcb582e087e9f0ac3
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-04-01 16:47:21 -0300

    change test strategy to use jest mocks

--- tests/data/services/github-authentication.spec.ts
+++ tests/data/services/github-authentication.spec.ts
@@ -1,32 +1,24 @@
 import { AuthenticationError } from '@/domain/errors'
-import { LoadGithubApi } from '@/data/contracts/apis'
 import { GithubAuthenticationService } from '@/data/services'
-
-class LoadGithubApiSpy implements LoadGithubApi {
-  code?: string
-  callsCount = 0
-  result = undefined
-  async loadUser ({ code }: LoadGithubApi.Input): Promise<LoadGithubApi.Output> {
-    this.code = code
-    this.callsCount++
-    return this.result
-  }
-}
 
 describe('GithubAuthenticationService', () => {
   it('should call LoadGithubApi with correct input', async () => {
-    const loadGithubApi = new LoadGithubApiSpy()
+    const loadGithubApi = {
+      loadUser: jest.fn()
+    }
     const sut = new GithubAuthenticationService(loadGithubApi)
 
     await sut.perform({ code: 'any_code' })
 
-    expect(loadGithubApi.code).toBe('any_code')
-    expect(loadGithubApi.callsCount).toBe(1)
+    expect(loadGithubApi.loadUser).toHaveBeenCalledWith({ code: 'any_code' })
+    expect(loadGithubApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('should return AuthenticationError when LoadGithubApi returns undefined', async () => {
-    const loadGithubApi = new LoadGithubApiSpy()
-    loadGithubApi.result = undefined
+    const loadGithubApi = {
+      loadUser: jest.fn()
+    }
+    loadGithubApi.loadUser.mockResolvedValueOnce(undefined)
     const sut = new GithubAuthenticationService(loadGithubApi)
 
     const authResult = await sut.perform({ code: 'any_code' })
