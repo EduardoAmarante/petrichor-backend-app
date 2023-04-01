be930fea62c1da99d3f0d00f6a79ec32deb9d19c
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-04-01 16:56:26 -0300

    simplify code

--- tests/data/services/github-authentication.spec.ts
+++ tests/data/services/github-authentication.spec.ts
@@ -5,25 +5,30 @@ import { LoadGithubApi } from '@/data/contracts/apis'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('GithubAuthenticationService', () => {
+  let code: string
   let loadGithubApi: MockProxy<LoadGithubApi>
   let sut: GithubAuthenticationService
 
-  beforeEach(() => {
+  beforeAll(() => {
+    code = 'any_code'
     loadGithubApi = mock<LoadGithubApi>()
+  })
+
+  beforeEach(() => {
     sut = new GithubAuthenticationService(loadGithubApi)
   })
 
   it('should call LoadGithubApi with correct input', async () => {
     await sut.perform({ code: 'any_code' })
 
-    expect(loadGithubApi.loadUser).toHaveBeenCalledWith({ code: 'any_code' })
+    expect(loadGithubApi.loadUser).toHaveBeenCalledWith({ code })
     expect(loadGithubApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('should return AuthenticationError when LoadGithubApi returns undefined', async () => {
     loadGithubApi.loadUser.mockResolvedValueOnce(undefined)
 
-    const authResult = await sut.perform({ code: 'any_code' })
+    const authResult = await sut.perform({ code })
 
     expect(authResult).toEqual(new AuthenticationError())
   })
