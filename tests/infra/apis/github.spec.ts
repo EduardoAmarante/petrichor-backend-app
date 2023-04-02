4a20f41af180796a12dd401ba0f0373c04f208b4
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-04-02 11:49:34 -0300

    ensure GitHubApi get token with correct params

--- tests/infra/apis/github.spec.ts
+++ tests/infra/apis/github.spec.ts
@@ -4,11 +4,20 @@ import { mock } from 'jest-mock-extended'
 
 class GitHubApi {
   constructor (
-    private readonly httpClient: HttpGetClient
+    private readonly httpClient: HttpGetClient,
+    private readonly clientId: string,
+    private readonly clientSecret: string
   ) {}
 
   async loadUser ({ code }: LoadGithubApi.Input): Promise<void> {
-    await this.httpClient.get({ url: 'https://github.com/login/oauth/access_token' })
+    await this.httpClient.get({
+      url: 'https://github.com/login/oauth/access_token',
+      params: {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        code
+      }
+    })
   }
 }
 
@@ -19,18 +28,34 @@ interface HttpGetClient {
 export namespace HttpGetClient {
   export type Input = {
     url: string
+    params: object
   }
 }
 
 describe('GitHubApi', () => {
+  let code: string
+  let clientId: string
+  let clientSecret: string
+
+  beforeAll(() => {
+    code = 'any_github_token'
+    clientId = 'any_client_id'
+    clientSecret = 'any_client_secret'
+  })
+
   it('should get github token', async () => {
     const httpClient = mock<HttpGetClient>()
-    const sut = new GitHubApi(httpClient)
+    const sut = new GitHubApi(httpClient, clientId, clientSecret)
 
-    await sut.loadUser({ code: 'any_github_code' })
+    await sut.loadUser({ code })
 
     expect(httpClient.get).toHaveBeenLastCalledWith({
-      url: 'https://github.com/login/oauth/access_token'
+      url: 'https://github.com/login/oauth/access_token',
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        code
+      }
     })
   })
 })
