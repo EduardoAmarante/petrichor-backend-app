759a985df9364981bc2e5e4bcd7a285e0e0f22e3
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-04-05 20:38:46 -0300

    ensure GithubLoginController calls GitHubAuthentication with correct input

--- tests/application/controllers/github-login.spec.ts
+++ tests/application/controllers/github-login.spec.ts
@@ -1,5 +1,14 @@
+import { GitHubAuthentication } from '@/domain/usecases'
+
+import { MockProxy, mock } from 'jest-mock-extended'
+
 class GithubLoginController {
+  constructor (
+    private readonly githubAuth: GitHubAuthentication
+  ) {}
+
   async handle (httpRequest: any): Promise<HttpResponse> {
+    await this.githubAuth.perform({ code: httpRequest.code })
     return {
       statusCode: 400,
       data: new Error('The field code is required')
@@ -13,10 +22,15 @@ type HttpResponse = {
 }
 
 describe('GithubLoginController', () => {
+  let githubAuth: MockProxy<GitHubAuthentication>
   let sut: GithubLoginController
 
+  beforeAll(() => {
+    githubAuth = mock()
+  })
+
   beforeEach(() => {
-    sut = new GithubLoginController()
+    sut = new GithubLoginController(githubAuth)
   })
 
   it('shoul return 400 if code is empty', async () => {
@@ -44,5 +58,12 @@ describe('GithubLoginController', () => {
       statusCode: 400,
       data: new Error('The field code is required')
     })
+  })
+
+  it('shoul call GithubAuthentication with correct input', async () => {
+    await sut.handle({ code: 'any_code' })
+
+    expect(githubAuth.perform).toHaveBeenCalledWith({ code: 'any_code' })
+    expect(githubAuth.perform).toHaveBeenCalledTimes(1)
   })
 })
