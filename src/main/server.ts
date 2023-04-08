2cd9c1bbdd9c5d30b0e370b75285c981a534e2d7
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-04-07 22:16:08 -0300

    add db initialize on server.ts

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,7 +2,12 @@ import './config/module-alias'
 
 import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
+import { db } from '@/infra/typeorm'
 
-app.listen(env.port, () => {
-  console.log(`🚀 Server is running at http://localhost:${env.port}`)
-})
+db.initialize()
+  .then(() => {
+    app.listen(env.port, () => {
+      console.log(`🚀 Server is running at http://localhost:${env.port}`)
+    })
+  })
+  .catch(() => console.error)
